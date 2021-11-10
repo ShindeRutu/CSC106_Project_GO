c81e17aeae918687d2ab6c1038cfd3ea9d8927f2
unknown <shinderutu1829@gmail.com>
2021-11-10 11:16:41 +0530

    Board : changes in the board size tht will be displayed on console

--- GO_Game/src/csc_GoGame_2115_2128/com/Board.java
+++ GO_Game/src/csc_GoGame_2115_2128/com/Board.java
@@ -14,60 +14,67 @@ public class Board {
 		 * to print the first line of alphabets 
 		 * ----------------------------------------------------*/
 		
-		for(col=0; col < boardSize/10+2; col++)
-			System.out.print(" ");
+
+		for(col=0; col< boardSize/10+2; col++)
+			System.out.print("   ");             // to get the 1st line of alpha in center of console
 		for (col =0; col< boardSize; col++)
-			System.out.print(" "+alpha.charAt(col));   // will display char at ith index
+			System.out.print("      " + alpha.charAt(col)+"");   // will display char at ith index
 		System.out.print("\n");
-	
-		// to print +-------+
-		for(i=0; i < boardSize/10+1; i++)   
-			System.out.print(" ");
-		System.out.print("+");   // prints "+" at the first pos 
-		for (i =0; i< boardSize; i++)   //"boardSize*2" is adjusted as per display 
-			System.out.print("--");  // prints --
-		System.out.print("-+");   // prints "+" at the last pos 
-	
-	
-		/* -------------------------------------------------------------------
-		 * to print the coordinates intersection(.)
-    	------------------------------------------------------------------------*/
-	
-		System.out.print("\n");  //to tke numbering on next line
-		for(i=1 ; i<=boardSize; i++)
-		{
-			// to print the row number at left side
-			for (row = 1; row < boardSize/11-i/11; row++) 
-				System.out.print(" "); // to print trailing spaces
-			System.out.print(i+"|");
-	    
-			for(j=0;j<boardSize; j++) 
-				System.out.print(" .");
-	    
-			// to print the row number at right side
-			System.out.println(" |" + i); 
-			System.out.print("");
-		}
-	
-		/* -------------------------------------------------------------------
-		 * to print the last line of  "+--------------+" and the alphabets
-    	-----------------------------------------------------------------------*/
 		
 		// to print + -------+
 		for(i=0; i < boardSize/10+1; i++)   
-			System.out.print(" ");
+			System.out.print("\t");
 		System.out.print("+");   // prints "+" at the first pos 
-		for (i =0; i< boardSize*2; i++)   //"boardSize*2" is adjusted as per display 
-			System.out.print("-");  // prints --
+		for (i =0; i< boardSize*3 + 4 ; i++)   //for loop "boardSize*3 + 4" is adjusted as per display 
+			System.out.print("--");  // prints --
 		System.out.print("-+");   // prints "+" at the last pos 
-	
+		
+		
+		/* -------------------------------------------------------------------
+        * to print the coordinates intersection(.)
+        -----------------------------------------------------*/
+		
+		System.out.print("\n");  //to tke numbering on next line
+		for(i=1 ; i<=boardSize; i++)
+		{
+			// to print the gap n between
+			System.out.println("        | \t\t\t\t\t\t\t\t|");
+			
+			// to print the row number at left side
+			for (row = 1; row < boardSize/11-i/11; row++) 
+				System.out.print("\t\t"); // to print trailing spaces
+			System.out.print("       "+ i+"|");
+					
+			for(j=0;j<boardSize; j++) 
+				System.out.print("   .   ");
+				    
+		    // to print the row number at right side
+			System.out.println("|" + i);  
+		}
+		
+		// to print the gap n between
+		System.out.println("        | \t\t\t\t\t\t\t\t|");
+		
+		/* -------------------------------------------------------------------
+	    * to print the last line of  "+--------------+" and the alphabets
+	    -----------------------------------------*/
+		
+		// to print + -------+
+		for(i=0; i < boardSize/10+1; i++)   
+			System.out.print("\t");
+		System.out.print("+");   // prints "+" at the first pos 
+		for (i =0; i< boardSize*3 +4; i++)   //"boardSize*3 + 4" is adjusted as per display 
+			System.out.print("--");  // prints --
+		System.out.print("-+");   // prints "+" at the last pos 
+		
 		System.out.print("\n");  
-		//to print the alpha bottom line
-		for(i=0; i < boardSize/10+2; i++)
-			System.out.print(" ");
-		for (i =0; i< boardSize; i++)
-			System.out.print(" "+alpha.charAt(i));   // will display char at ith index
+		//to print the alphas
+		for(col=0; col<boardSize/10+2; col++)
+			System.out.print("   ");
+		for (col=0; col< boardSize; col++)
+			System.out.print("      " + alpha.charAt(col)+"");    // will display char at ith index
 		System.out.print("\n");
+		
     
 	}//end of printBoard
     
